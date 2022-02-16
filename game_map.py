141cc89336e71bf9bc2546ee4150901461874b0a
Omega-Who <58758022+Omega-Who@users.noreply.github.com>
2022-02-16 14:42:44 -0800

    part 12

--- game_map.py
+++ game_map.py
@@ -112,8 +112,6 @@ class GameWorld:
         max_rooms: int,
         room_min_size: int,
         room_max_size: int,
-        max_monsters_per_room: int,
-        max_items_per_room: int,
         current_floor: int = 0
     ):
         self.engine = engine
@@ -125,9 +123,6 @@ class GameWorld:
 
         self.room_min_size = room_min_size
         self.room_max_size = room_max_size
-
-        self.max_monsters_per_room = max_monsters_per_room
-        self.max_items_per_room = max_items_per_room
 
         self.current_floor = current_floor
 
@@ -142,7 +137,5 @@ class GameWorld:
             room_max_size=self.room_max_size,
             map_width=self.map_width,
             map_height=self.map_height,
-            max_monsters_per_room=self.max_monsters_per_room,
-            max_items_per_room=self.max_items_per_room,
             engine=self.engine,
         )
